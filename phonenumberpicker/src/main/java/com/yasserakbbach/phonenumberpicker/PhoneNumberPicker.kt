f83096a61fd625f8aa0c2dc7737a5bf3ed5b2150
Dmytro Lashyn <dmytro.lashyn@meest.com>
2025-06-10 14:44:37 +0300

    Fixed the pair issue

--- phonenumberpicker/src/main/java/com/yasserakbbach/phonenumberpicker/PhoneNumberPicker.kt
+++ phonenumberpicker/src/main/java/com/yasserakbbach/phonenumberpicker/PhoneNumberPicker.kt
@@ -5,7 +5,6 @@ import android.graphics.Color
 import android.text.InputFilter
 import android.util.AttributeSet
 import android.util.Log
-import android.util.Pair
 import android.util.TypedValue
 import android.view.KeyEvent
 import android.view.LayoutInflater
